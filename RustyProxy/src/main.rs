149ae2dd1bc7914a8cdd1266f51c7458845f679c
JeffSsh <96548634+WorldSsh@users.noreply.github.com>
2025-04-02 12:22:10 -0300

    Update main.rs

--- RustyProxy/src/main.rs
+++ RustyProxy/src/main.rs
@@ -50,6 +50,14 @@ async fn handle_client(mut client_stream: TcpStream) -> Result<(), Error> {
     .write_all(format!("HTTP/1.1 101 {}\r\n\r\n", status).as_bytes())
     .await?;
 
+    client_stream
+        .write_all(format!("HTTP/1.1 200 {}\r\n\r\n", status).as_bytes())
+        .await?;
+    
+    client_stream
+        .write_all(format!("HTTP/1.1 200 {}\r\n\r\n", status).as_bytes())
+        .await?;
+
     let mut addr_proxy = "0.0.0.0:22";
     let result = timeout(Duration::from_secs(1), peek_stream(&mut client_stream)).await
         .unwrap_or_else(|_| Ok(String::new()));
