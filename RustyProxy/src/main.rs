610842df4a5c0318aa6be9c8be86d4a30bc929e9
JeffSsh <96548634+WorldSsh@users.noreply.github.com>
2025-04-07 21:00:38 -0300

    Update main.rs

--- RustyProxy/src/main.rs
+++ RustyProxy/src/main.rs
@@ -1,9 +1,7 @@
 use std::env;
 use std::io::Error;
-use std::sync::Arc;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, copy_bidirectional};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::Mutex;
 use tokio::time::{timeout, Duration};
 
 #[tokio::main]
@@ -52,13 +50,13 @@ async fn handle_client(mut client_stream: TcpStream) -> Result<(), Error> {
         .write_all(format!("HTTP/1.1 200 {}\r\n\r\n", status).as_bytes())
         .await?;
 
-    let addr_proxy = match timeout(Duration::from_secs(1), peek_stream(&mut client_stream)).await {
+    let addr_proxy = match timeout(Duration::from_secs(5), peek_stream(&mut client_stream)).await {
         Ok(Ok(data)) if data.contains("SSH") || data.is_empty() => "0.0.0.0:22",
         Ok(_) => "0.0.0.0:1194",
         Err(_) => "0.0.0.0:22",
     };
 
-    let server_stream = match TcpStream::connect(addr_proxy).await {
+    let mut server_stream = match TcpStream::connect(addr_proxy).await {
         Ok(stream) => stream,
         Err(_) => {
             eprintln!("Erro ao conectar-se ao servidor proxy em {}", addr_proxy);
@@ -66,42 +64,9 @@ async fn handle_client(mut client_stream: TcpStream) -> Result<(), Error> {
         }
     };
 
-    let (client_read, client_write) = client_stream.into_split();
-    let (server_read, server_write) = server_stream.into_split();
+    // Transfere dados entre cliente e servidor com buffer dinâmico
+    let _ = copy_bidirectional(&mut client_stream, &mut server_stream).await;
 
-    let client_read = Arc::new(Mutex::new(client_read));
-    let client_write = Arc::new(Mutex::new(client_write));
-    let server_read = Arc::new(Mutex::new(server_read));
-    let server_write = Arc::new(Mutex::new(server_write));
-
-    tokio::try_join!(
-        transfer_data(client_read.clone(), server_write.clone()),
-        transfer_data(server_read.clone(), client_write.clone())
-    )?;
-
-    Ok(())
-}
-
-async fn transfer_data(
-    read_stream: Arc<Mutex<tokio::net::tcp::OwnedReadHalf>>,
-    write_stream: Arc<Mutex<tokio::net::tcp::OwnedWriteHalf>>,
-) -> Result<(), Error> {
-    let mut buffer = [0; 8192];
-    loop {
-        let bytes_read = {
-            let mut reader = read_stream.lock().await;
-            match reader.read(&mut buffer).await {
-                Ok(0) => break, 
-                Ok(n) => n,
-                Err(_) => break,
-            }
-        };
-
-        let mut writer = write_stream.lock().await;
-        if writer.write_all(&buffer[..bytes_read]).await.is_err() {
-            break;
-        }
-    }
     Ok(())
 }
 
